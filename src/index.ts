5cb8f46b98afb1515a88a22e4ae27b565910b87e
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-09-13 22:33:48 +0300

    setup config

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import 'dotenv/config'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
+
+app.use ('/*', cors())
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
 
 serve({
   fetch: app.fetch,
-  port
+  port: parseInt(process.env.PORT || '3000')
 })
+
+console.log(`Server is running on port ${process.env.PORT}`)
+
