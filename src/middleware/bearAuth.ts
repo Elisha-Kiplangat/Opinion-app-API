3289aa4eb751f40a08c8fa4be356f6719385fae4
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-09-20 11:59:35 +0300

    authorization

---
+++ src/middleware/bearAuth.ts
@@ -0,0 +1,43 @@
+
+import { Context, Next } from "hono";
+import { verify } from "hono/jwt";
+
+export const verifyToken = async (token: string, secret: string) => {
+    try {
+        const decoded = await verify(token as string, secret)
+        return decoded;
+    } catch (error: any) {
+        return error
+    }
+}
+
+export const authMiddleware = async (c: Context, next: Next, requiredRole: string) => {
+    const token = c.req.header("Authorization");
+
+    if (!token) return c.json({err: "Token not provided"}, 401);
+
+    const decoded = await verifyToken(token, process.env.JWT_SECRET as string);
+
+    if (!decoded) return c. json({error: "Invalid token"}, 401);
+
+    if (decoded.role !== requiredRole) return c.json({error: "Unauthorized"}, 401);
+
+    return  next();
+}
+
+export const allAuthMiddleware = async (c: Context, next: Next, requiredRole: string) => {
+    const token = c.req.header("Authorization");
+
+    if (!token) return c.json({ error: "Token not provided" }, 401);
+
+    const decoded = await verifyToken(token, process.env.JWT_SECRET as string);
+
+    if (!decoded) return c.json({ error: "Invalid token" }, 401);
+
+    return next();
+}
+
+export const adminRoleAuth = async (c: Context, next: Next) => await authMiddleware(c, next, "admin")
+export const partnerRoleAuth = async (c: Context, next: Next) => await authMiddleware(c, next, "partner")
+export const userRoleAuth = async (c: Context, next: Next) => await authMiddleware(c, next, "user")
+export const allRoleAuth = async (c: Context, next: Next) => await allAuthMiddleware(c, next, "all")
